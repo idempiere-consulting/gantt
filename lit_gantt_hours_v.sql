016530f4c91d4a552b04d4fa70be4e58650a06ba
maramau <mauro.biasutti@gmail.com>
2021-02-13 18:14:04 +0100

    aggiunge definizione vista per le attività

---
+++ lit_gantt_hours_v.sql
@@ -0,0 +1,52 @@
+ -- drop VIEW lit_gantt_hours_v  CASCADE;
+create or replace view lit_gantt_hours_v as 
+SELECT 10000000::numeric + h.lit_hour_id AS lit_gantt_hours_v_id,
+    10000000::numeric + h.lit_hour_id AS id,
+    h.name AS text,
+    h.dateworkstart AS start_date1,
+    h.dateworkstart + '01:00:00'::interval hour * h.qty::double precision AS end_date1,
+    to_char(h.dateworkstart, 'yyyy-mm-dd hh24:mi:ss'::text) AS start_date,
+    to_char(h.dateworkstart + '01:00:00'::interval hour * h.qty::double precision, 'yyyy-mm-dd hh24:mi:ss'::text) AS end_date,
+    h.salesrep_id AS owner,
+    h.salesrep_id AS s_resource_id,
+    h.c_contactactivity_id + 90000000::numeric AS parent,
+        CASE
+            WHEN h.isconfirmed = 'Y'::bpchar THEN '1'::text
+            WHEN h.isconfirmed = 'N'::bpchar THEN '0'::text
+            ELSE '0.5'::text
+        END AS progress,
+    'lit_hour'::text AS table_from,
+    'task'::text AS type,
+    '0'::text AS sortorder,
+    'Y'::text AS isactive,
+    1000006 AS ad_client_id,
+    h.updated,
+    h.updatedby
+   FROM lit_hour h
+  WHERE h.name IS NOT NULL AND h.dateworkstart > (now() - 1::numeric)
+UNION
+ SELECT 90000000::numeric + c.c_contactactivity_id AS lit_gantt_hours_v_id,
+    90000000::numeric + c.c_contactactivity_id AS id,
+    (c.documentno::text || ' '::text) || c.name::text AS text,
+    c.startdate AS start_date1,
+    c.enddate AS end_date1,
+    to_char(c.startdate, 'yyyy-mm-dd hh24:mi:ss'::text) AS start_date,
+    to_char(c.enddate, 'yyyy-mm-dd hh24:mi:ss'::text) AS end_date,
+    c.salesrep_id AS owner,
+    c.salesrep_id AS s_resource_id,
+    0 AS parent,
+    '0.5'::text AS progress,
+    'c_contactactivity'::text AS table_from,
+    'project'::text AS type,
+    '0'::text AS sortorder,
+    'Y'::text AS isactive,
+    1000006 AS ad_client_id,
+    c.updated,
+    c.updatedby
+   FROM c_contactactivity c
+     JOIN r_status r ON c.r_status_id = r.r_status_id
+  WHERE c.name IS NOT NULL;
+-- è agganciato il trigger per "scivere" sulla vista, verificare funzionamento  
+--Triggers:
+  --  test_it2 INSTEAD OF UPDATE ON lit_gantt_hours_v FOR EACH ROW EXECUTE FUNCTION test_trigger()
+
