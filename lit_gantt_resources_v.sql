71c6da17c69a37c458c5026341396e6b4c64b73a
idempiere consulting <58611269+idempiere-consulting@users.noreply.github.com>
2021-03-02 19:26:46 +0100

    Update lit_gantt_resources_v.sql

--- lit_gantt_resources_v.sql
+++ lit_gantt_resources_v.sql
@@ -1,23 +1,23 @@
   drop VIEW lit_gantt_resources_v  CASCADE;
 create or replace view lit_gantt_resources_v as 
 SELECT s_resource.s_resource_id AS lit_gantt_resources_v_id,
-    s_resource.s_resource_id AS key,
-    s_resource.value AS label,
-    s_resource.name,
-    s_resource.s_resourcetype_id,
-    s_resource.m_warehouse_id,
-    s_resource.isavailable,
-    s_resource.ad_client_id,
-    s_resource.ad_org_id,
-    s_resource.created,
-    s_resource.updated,
-    s_resource.createdby,
-    s_resource.updatedby,
-    s_resource.isactive,
+    r.s_resource_id AS key,
+    r.value AS label,
+    r.name,
+    r.s_resourcetype_id,
+    r.m_warehouse_id,
+    r.isavailable,
+    r.ad_client_id,
+    r.ad_org_id,
+    r.created,
+    r.updated,
+    r.createdby,
+    r.updatedby,
+    r.isactive,
     ''::text AS parent,
     t.DailyCapacity AS dailycapacity
-   FROM s_resource s_resource
-   JOIN s_resource     r on r.s_resource_id     = s_resource.s_resource_id
+   FROM s_resource r
+ 
    JOIN s_resourcetype t on t.s_resourcetype_id = r.s_resourcetype_id
 
   WHERE s_resource.ad_client_id = 1000006::numeric;
