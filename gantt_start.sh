7fe3bbc009ad3bc7de194d2847f441e345a8840e
maramau <mauro.biasutti@gmail.com>
2021-03-04 17:19:41 +0100

    migliora la parametrizzazione del servizio

--- gantt_start.sh
+++ gantt_start.sh
@@ -1,8 +1,13 @@
 python --version
-#cd /root/gantt
+# assume l'esistenza di una variabile di nome
+# GANTTHOME=directory/di/base/
+# il posto più comodo su un server è /etc/environment (system wide)
+# in "casa" forse è più comodo nel .bashrc
+cd $GANTTHOME
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export FLASK_ENV=development
+#export FLASK_APP=api_ilserver-risorse.py
 export FLASK_APP=api_ilserver-activity.py
 . ./gantV/bin/activate
 python --version
