9aefbf831bf5fee2b39b44b74e635de7b10d12b8
maramau <mauro.biasutti@gmail.com>
2021-03-04 18:26:39 +0100

    corregge errori bloccanti all'avvio

--- gantt_start.sh
+++ gantt_start.sh
@@ -3,12 +3,15 @@ python --version
 # GANTTHOME=directory/di/base/
 # il posto più comodo su un server è /etc/environment (system wide)
 # in "casa" forse è più comodo nel .bashrc
+echo $GANTTHOME
+
 cd $GANTTHOME
+pwd
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export FLASK_ENV=development
-#export FLASK_APP=api_ilserver-risorse.py
-export FLASK_APP=api_ilserver-activity.py
+export FLASK_APP=api_ilserver-risorse.py
+#export FLASK_APP=api_ilserver-activity.py
 . ./gantV/bin/activate
 python --version
-flask run --host=127.0.0.1 --port=8090
+flask run --host=127.0.0.1 --port=8091
